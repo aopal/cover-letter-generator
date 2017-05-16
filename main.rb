9fabf1fb4ebad438e6fa480d87137e7d48161f14
Anuj Opal <anujopal@gmail.com>
2017-05-16 12:37:25 -0400

    job search and text extraction

--- main.rb
+++ main.rb
@@ -2,25 +2,58 @@ require 'pdfkit'
 require 'combine_pdf'
 require 'watir'
 
+wkhtml = "wkhtmltopdf.exe"
+priv_key = "C:\\keys\\priv"
+enc_file = "enc"
+rsa = "..\\RSA-Electron\\main.js"
+
+content = `node #{rsa} -d #{priv_key} #{enc_file}`
+user = content.split("\n").first
+pass = content.split("\n").last.split("\x00").first
+
 PDFKit.configure do |config|
-  config.wkhtmltopdf = "wkhtmltopdf.exe"
+  config.wkhtmltopdf = wkhtml
   config.default_options = {
     :page_size => 'Letter',
     :print_media_type => true
   }
 end
 
-parent = Watir::Browser.new :phantomjs
+parent = Watir::Browser.new :chrome
+parent.goto "waterlooworks.uwaterloo.ca"
+parent.link(text: "Students/Staff").click
+parent.input(id: "username").send_keys(user)
+parent.input(id: "password").send_keys(pass)
+parent.input(name: "submit").click
+parent.link(text: "Hire Waterloo Co-op").click
+parent.input(id: "postingId").send_keys(ARGV[0])
+parent.form(id: "searchByPostingNumberForm").link.click
 
-kit = PDFKit.new(File.new('base.html'))
+position = parent.tr(text: /job title/i).text
+company = parent.tr(text: /organization/i).text
 
-file = kit.to_file('base2.pdf')
+searchText = parent.tr(text: /job summary/i).text
+searchText += "\n\n" + parent.tr(text: /job responsibilities/i).text
+searchText += "\n\n" + parent.tr(text: /required skills/i).text
+searchText.downcase!
 
-fileName = ARGV[0]
-text = ""
-file = File.new(fileName, "r")
+puts position, company
 
-while(line = file.gets)
-  puts line.length()
-  text += line + "\n"
-end
+sleep 5
+
+puts searchText
+
+sleep(60)
+
+# kit = PDFKit.new(File.new('base.html'))
+
+# file = kit.to_file('base2.pdf')
+
+# fileName = ARGV[0]
+# text = ""
+# file = File.new(fileName, "r")
+# 
+# while(line = file.gets)
+#   puts line.length()
+#   text += line + "\n"
+# end
